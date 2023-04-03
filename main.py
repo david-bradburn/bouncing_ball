554dca1a8fbc13bb3d95c6ec170d235baf604514
David Bradburn <david.bradburn2@gmail.com>
2023-04-03 14:50:37 +0100

    Finish with animation

--- main.py
+++ main.py
@@ -4,30 +4,60 @@
 
 # import pygame
 # pygame.init()
+import time
 
 class World():
     def __init__(self) -> None:
-        self.Height = 50 ## Might want to generate custom world rather than a rectangle
-        self.Width  = 100
+        self.Height = 20 ## Might want to generate custom world rather than a rectangle
+        self.Width  = 30
 
-        self.ball = Ball()
+        self.ball = Ball(2, 3)
+
+        self.main()
 
     def main(self):
-
+        self.display()
+        n = 0
         while(True):
-            new_x = self.ball.x + self.ball.vx
-            new_y = self.ball.y + self.ball.vy
+            time.sleep(0.1)
+            n += 1
+            self.ball.x += self.ball.vx
+            self.ball.y += self.ball.vy
 
-            if new_x == (self.Width - 1) or new_x == 0:
+            if self.ball.x == (self.Width - 1) or self.ball.x == 0:
                 self.ball.vx = -self.ball.vx
             
-            if new_y == (self.Height - 1) or new_y == 0:
-                self.ball.vy = self.ball.vy
+            if self.ball.y == (self.Height - 1) or self.ball.y == 0:
+                self.ball.vy = -self.ball.vy
+            
 
 
-    def display(self):
-        for y
-        pass
+            self.display()
+            
+
+    def screen_clear(self):
+
+        LINE_UP = '\033[1A'
+        LINE_CLEAR = '\x1b[2K'
+
+        for i in range(self.Height + 1):
+            print(LINE_UP, end=LINE_CLEAR)
+
+    def display(self):#|-+
+        display_str = ""
+        for y_coord in range(self.Height):
+            str_temp = ""
+            for x_coord in range(self.Width):
+                if x_coord == self.ball.x and y_coord == self.ball.y:
+                    str_temp += "X"
+                else:
+                    str_temp += " "
+
+            display_str = str_temp + "\n" + display_str
+
+            # print()
+        self.screen_clear()
+        print(display_str)
 
 
 class Ball():
@@ -37,5 +67,6 @@ class Ball():
         self.vx = initial_vx #
         self.vy = initial_vy
 
-    
+
+g = World()    
 
